4d2b03281363fb224ca58dd77aaa605a25b8e93c
Baoz <baoz@mark24.local>
2017-07-31 18:03:10 +0800

    解决如何dll打包的问题并将webpack文分成prod和dev的

--- README.md
+++ README.md
@@ -5,8 +5,9 @@
 加入了redux-obervable中间件    
 使用css-module书写样式； 
  
-具体文件参见[docs目录下的文档](./docs/README.md)  
+具体文件参见[docs目录下的文档](./docs/README.md)
 ps：写得很lower，希望您能留下宝贵的意见；  
+ps： 已废弃~~
 
 邮箱：yangtuo0730@gmail.com
 
@@ -16,13 +17,13 @@ ps：写得很lower，希望您能留下宝贵的意见；
 任待解决的问题：
 
 - 这个demo要做什么
-- dll打包的问题
 - webpack升级到3
 
 
 已解决的问题：
 
 - eslint规范代码
+- 解决如何dll打包的问题，并将webpack文分成prod和dev的
 
 
 
---
+++ webpack.base.config.js
@@ -0,0 +1,70 @@
+const webpack = require('webpack')
+const path = require('path')
+const rootPath = path.resolve(__dirname)
+const ROOT_PATH = path.resolve(__dirname)
+const HtmlwebpackPlugin = require('html-webpack-plugin')
+
+const config = {
+    entry: path.resolve(__dirname, 'src/app.js'),
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.bundle.js',
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                enforce: "pre",
+                loader: "eslint-loader",
+                exclude: /node_modules/,
+                options: {
+                    emitWarning: true,
+                    emitError: false,
+                    //failOnWarning: false,
+                    //failOnError: true,
+                    useEslintrc: false,
+                    configFile: path.join(__dirname, ".eslintrc")
+                }
+            },{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react', "stage-0"],
+                    plugins: [
+                        "transform-object-rest-spread",
+                        "transform-decorators-legacy", ["import", {
+                            "libraryName": "antd",
+                            "style": true
+                        }]
+                    ]
+                }
+            }, {
+                test: /\.css$/,
+                loader: ['style-loader', 'css-loader'],
+            }, {
+                test: /\.less$/,
+                exclude: [
+                    path.resolve(__dirname, 'src/styles'),
+                    path.resolve(__dirname, 'node_modules')
+                ],
+                loader: 'style-loader!css-loader?modules&localIdentName=[name]__[local]!less-loader?sourceMap=true'
+            }, {
+                test: /\.less$/,
+                include: [path.resolve(__dirname, 'src/styles'),
+                    path.resolve(__dirname, 'node_modules')],
+                loader: 'style-loader!css-loader!less-loader?sourceMap=true'
+            }
+        ],     
+    },
+    plugins: [
+        new webpack.DllReferencePlugin({
+            manifest: require(path.resolve(ROOT_PATH, 'manifest.json')),
+            context: ROOT_PATH,
+        })
+    ]
+}
+
+module.exports = config
